54e504db36459b6032f5fc7f6d898013d8254b82
ChesterNavas <67553894+ChesterNavas@users.noreply.github.com>
2020-06-28 12:45:31 -0600

    Add files via upload

---
+++ map/map-markers.js
@@ -0,0 +1,357 @@
+function getMarkers() {
+var obj = [
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"2019", 
+ "max":"2019", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR--ALMERE--HOLANDA", 
+ "lko":"ALMERE", 
+ "cty":"Almere", 
+ "text":"Ciudad the Almere, Holanda (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: CABRERA NAVAS.;1 eventos: 1 nacimientos.;se ha producido el \"6 Oct 2019\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"x", 
+ "d":"3", 
+ "min":"1951", 
+ "max":"1951", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR--INSTITUTO-NACIONAL-DE-ORIENTE--GRANADA--NICARAGUA---GRANADA--GRANADA--NICARAGUA", 
+ "lko":"INSTITUTO-NACIONAL-DE-ORIENTE", 
+ "cty":"Instituto Nacional de Oriente", 
+ "text":"Ciudad the Instituto Nacional de Oriente, Nicaragua (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: Navas Navas.;1 eventos: 1 otros.;se ha producido el \"Ene 1951\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"2019", 
+ "max":"2019", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR--LOS-ANGELES--CALIFORNIA-------", 
+ "lko":"LOS-ANGELES", 
+ "cty":"Los Angeles", 
+ "text":"Ciudad the Los Angeles,  (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: CUADRA CABRERA.;1 eventos: 1 nacimientos.;se ha producido el \"29 Jun 2019\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"o", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1981", 
+ "max":"1981", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----------ESPANA", 
+ "lko":"ESPANA", 
+ "cty":"España", 
+ "text":"Ciudad the España, España (N45.0 W4.0).;1 individuos: 1 otros.;Apellidos más comunes: Marro Larrañaga.;1 eventos: 1 nacimientos.;se ha producido el \"16 May 1981\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"o", 
+ "t":"d", 
+ "d":"3", 
+ "min":"2004", 
+ "max":"2004", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----------GRANADA", 
+ "lko":"GRANADA", 
+ "cty":"GRANADA", 
+ "text":"Ciudad the GRANADA, GRANADA (N45.0 W4.0).;1 individuos: 1 otros.;Apellidos más comunes: Navas Guerrero.;1 eventos: 1 muertes.;se ha producido el \"7 Jul 2004\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1902", 
+ "max":"2008", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----------NICARAGUA", 
+ "lko":"NICARAGUA", 
+ "cty":"Nicaragua", 
+ "text":"Ciudad the Nicaragua, Nicaragua (N45.0 W4.0).;5 individuos: 2 primos, 3 otros.;Apellidos más comunes: CUADRA CABRERA, Cuadra Cabrera, Duarte, Duarte Lazo, Lazo.;5 eventos: 5 nacimientos.;se ha producido entre \"1902\" y \"26 Mar 2008\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"0", 
+ "max":"0", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------CHINANDEGA--NICARAGUA", 
+ "lko":"CHINANDEGA", 
+ "cty":"Chinandega", 
+ "text":"Ciudad the Chinandega, Nicaragua (N45.0 W4.0).;2 individuos: 1 primos, 1 otros.;Apellidos más comunes: Navas Sánchez, Sánchez.;2 eventos: 2 nacimientos.;" 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"h", 
+ "a":"s", 
+ "t":"bm", 
+ "d":"3", 
+ "min":"1897", 
+ "max":"2012", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------GRANADA--NICARAGUA", 
+ "lko":"GRANADA", 
+ "cty":"Granada", 
+ "text":"Ciudad the Granada, Nicaragua (N45.0 W4.0).;61 individuos: 1 ascendientes, 46 primos, 14 otros.;Apellidos más comunes: Navas Duarte, Navas Navas, Cabrera Navas, Correa Navas, NAVAS CASTILLO.;61 eventos: 59 nacimientos, 1 matrimonios, 1 otros.;se ha producido entre \"1897\" y \"9 Jul 2012\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"o", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1953", 
+ "max":"1953", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------LA-CEIBA--HONDURAS", 
+ "lko":"LA-CEIBA", 
+ "cty":"La Ceiba", 
+ "text":"Ciudad the La Ceiba, Honduras (N45.0 W4.0).;1 individuos: 1 otros.;Apellidos más comunes: Diaz Pizzaty.;1 eventos: 1 nacimientos.;se ha producido el \"25 Ago 1953\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"bd", 
+ "d":"3", 
+ "min":"1988", 
+ "max":"2007", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------MANAGUA--NICARAGUA", 
+ "lko":"MANAGUA", 
+ "cty":"Managua", 
+ "text":"Ciudad the Managua, Nicaragua (N45.0 W4.0).;2 individuos: 2 primos.;Apellidos más comunes: NAVAS LOPEZ, NAVAS RIVERA.;2 eventos: 1 nacimientos, 1 muertes.;se ha producido entre \"15 Feb 1988\" y \"25 Ago 2007\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"o", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1900", 
+ "max":"1900", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------MATAGALPA--NICARAGUA", 
+ "lko":"MATAGALPA", 
+ "cty":"Matagalpa", 
+ "text":"Ciudad the Matagalpa, Nicaragua (N45.0 W4.0).;1 individuos: 1 otros.;Apellidos más comunes: Salgado Almanza.;1 eventos: 1 nacimientos.;se ha producido el \"7 Jun 1900\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1999", 
+ "max":"2003", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------NUEVA-YORK--ESTADOS-UNIDOS", 
+ "lko":"NUEVA-YORK", 
+ "cty":"Nueva York", 
+ "text":"Ciudad the Nueva York, Estados Unidos (N45.0 W4.0).;2 individuos: 2 primos.;Apellidos más comunes: Crespo Navas.;2 eventos: 2 nacimientos.;se ha producido entre \"25 Oct 1999\" y \"27 Mar 2003\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"2008", 
+ "max":"2016", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------PAMPLONA--ESPANA", 
+ "lko":"PAMPLONA", 
+ "cty":"Pamplona", 
+ "text":"Ciudad the Pamplona, España (N45.0 W4.0).;3 individuos: 3 primos.;Apellidos más comunes: Navas Marro.;3 eventos: 3 nacimientos.;se ha producido entre \"18 Abr 2008\" y \"21 Dic 2016\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"o", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1974", 
+ "max":"1974", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------SAN-ANTONIO-DE-LOS-BANOS--CUBA", 
+ "lko":"SAN-ANTONIO-DE-LOS-BANOS", 
+ "cty":"San Antonio de los Baños", 
+ "text":"Ciudad the San Antonio de los Baños, Cuba (N45.0 W4.0).;1 individuos: 1 otros.;Apellidos más comunes: Leon.;1 eventos: 1 nacimientos.;se ha producido el \"2 Mar 1974\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"s", 
+ "t":"d", 
+ "d":"3", 
+ "min":"1994", 
+ "max":"2012", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------SAN-JOSE--COSTA-RICA", 
+ "lko":"SAN-JOSE", 
+ "cty":"San José", 
+ "text":"Ciudad the San José, Costa Rica (N45.0 W4.0).;2 individuos: 1 ascendientes, 1 otros.;Apellidos más comunes: Navas Guerrero, Navas Zambrana.;2 eventos: 2 muertes.;se ha producido entre \"30 Abr 1994\" y \"9 Oct 2012\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"m", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1956", 
+ "max":"2010", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------SAN-PEDRO-SULA--HONDURAS", 
+ "lko":"SAN-PEDRO-SULA", 
+ "cty":"San Pedro Sula", 
+ "text":"Ciudad the San Pedro Sula, Honduras (N45.0 W4.0).;10 individuos: 5 primos, 5 otros.;Apellidos más comunes: Diaz Torres, Navas Guerra, Guerra Benedetto, Navas García, Navas Gutierrez.;10 eventos: 10 nacimientos.;se ha producido entre \"14 May 1956\" y \"10 Sep 2010\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"m", 
+ "a":"c", 
+ "t":"bmd", 
+ "d":"3", 
+ "min":"1932", 
+ "max":"2013", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR---------TEGUCIGALPA--HONDURAS", 
+ "lko":"TEGUCIGALPA", 
+ "cty":"Tegucigalpa", 
+ "text":"Ciudad the Tegucigalpa, Honduras (N45.0 W4.0).;8 individuos: 1 primos, 7 otros.;Apellidos más comunes: Salgado, Hasbun Salgado, Luque Hasbun, Medina, Moreno.;8 eventos: 4 nacimientos, 1 matrimonios, 3 muertes.;se ha producido entre \"7 Mar 1932\" y \"21 Dic 2013\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"b", 
+ "d":"3", 
+ "min":"1933", 
+ "max":"1964", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-------GRANADA--DEPARTAMENTO-DE-GRANADA--NICARAGUA", 
+ "lko":"GRANADA", 
+ "cty":"Granada", 
+ "text":"Ciudad the Granada, Nicaragua (N45.0 W4.0).;2 individuos: 2 primos.;Apellidos más comunes: Correa Navas, NAVAS NAVAS.;2 eventos: 2 nacimientos.;se ha producido entre \"20 Feb 1933\" y \"5 Feb 1964\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"m", 
+ "a":"c", 
+ "t":"bmd", 
+ "d":"3", 
+ "min":"1978", 
+ "max":"1996", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-------SAN-PEDRO-SULA--DEPARTAMENTO-DE-CORTES--HONDURAS", 
+ "lko":"SAN-PEDRO-SULA", 
+ "cty":"San Pedro Sula", 
+ "text":"Ciudad the San Pedro Sula, Honduras (N45.0 W4.0).;9 individuos: 4 primos, 5 otros.;Apellidos más comunes: Navas Duarte, Duarte Lazo, Torres Salgado, Diaz Pizzaty, García.;7 eventos: 1 nacimientos, 3 matrimonios, 2 muertes, 1 otros.;se ha producido entre \"21 Jul 1978\" y \"13 Mar 1996\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"x", 
+ "d":"3", 
+ "min":"1964", 
+ "max":"1964", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----HOSPITAL-GENERAL-DE-MANAGUA---MANAGUA---DEPARTAMENTO-MANAGUA--NICARAGUA", 
+ "lko":"HOSPITAL-GENERAL-DE-MANAGUA-", 
+ "cty":"Hospital General de Managua.", 
+ "text":"Ciudad the Hospital General de Managua., Nicaragua (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: Navas Navas.;1 eventos: 1 otros.;se ha producido el \"Ene 1964\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"x", 
+ "d":"3", 
+ "min":"1965", 
+ "max":"1965", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----HOSPITAL-DE-GINECO-OBSTETRICIA-DEL-INSTITUTO-MEXICANO-DEL-SEGURO-SOCIAL--IMSS---CIUDAD-MEXICO---MEXICO", 
+ "lko":"HOSPITAL-DE-GINECO-OBSTETRICIA-DEL-INSTITUTO-MEXICANO-DEL-SEGURO-SOCIAL", 
+ "cty":"Hospital de Gineco-Obstetricia del Instituto Mexicano del Seguro Social", 
+ "text":"Ciudad the Hospital de Gineco-Obstetricia del Instituto Mexicano del Seguro Social, Mexico (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: Navas Navas.;1 eventos: 1 otros.;se ha producido el \"Feb 1965\"." 
+ },
+
+{
+ "x": "-4.0", 
+ "y":"45.0", 
+ "s":"l", 
+ "a":"c", 
+ "t":"x", 
+ "d":"3", 
+ "min":"1960", 
+ "max":"1960", 
+ "lkp":"citiesdetails_1.html", 
+ "lki":"LUGAR-----UNIVERSIDAD-NACIONAL-DE-NICARAGUA---LEON---DEPART--LEON--NICARAGUA", 
+ "lko":"UNIVERSIDAD-NACIONAL-DE-NICARAGUA-", 
+ "cty":"Universidad Nacional de Nicaragua.", 
+ "text":"Ciudad the Universidad Nacional de Nicaragua., Nicaragua (N45.0 W4.0).;1 individuos: 1 primos.;Apellidos más comunes: Navas Navas.;1 eventos: 1 otros.;se ha producido el \"1960\"." 
+ },
+
+];
+return obj;
+}
